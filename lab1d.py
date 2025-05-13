16afc33a0597254bd19d9c946cf1804bdff13734
faisal karim <fkarim22@myseneca.ca>
2025-05-12 20:36:05 -0400

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Description: This program is for some additional mathematical operation'''
+
+x = 10
+y = 2
+z = 5
+result = x + y * z
+print(f"{x} + {y} * {z} = {result}")
